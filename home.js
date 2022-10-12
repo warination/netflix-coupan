b17478f85bf6e82894f7cf09e943db161660fd22
warination <warination@gmail.com>
2022-10-12 10:42:55 +0530

    final submitjjjj

---
+++ home.js
@@ -0,0 +1,7 @@
+function loadCoupan(){
+    document.getElementById('coupan-container').style.visibility = 'visible';
+}
+
+function closeCoupan(){
+    document.getElementById('coupan-container').style.visibility = 'hidden';
+}
